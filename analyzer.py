3429197bdf8fe9380e64b91f1a6a944b4f03967c
Cheesehyvel <eric.hoglander@gmail.com>
2021-05-07 16:26:52 +0200

    Added CoE check

--- analyzer.py
+++ analyzer.py
@@ -7,26 +7,17 @@ class Analyzer:
         transport = AIOHTTPTransport(url='https://classic.warcraftlogs.com/api/v2/client', headers={'Authorization': 'Bearer '+token}, timeout=120)
         self.client = Client(transport=transport, fetch_schema_from_transport=True)
 
-    def run(self, zoneID, encounterID, sourceClass, limit = 10, pages = 1, page = 1, format=None, spellFilter=True):
+    def run(self, zoneID, encounterID, startTime = 0, limit = 10, pages = 1, page = 1, format=None, spellFilter=True):
 
-        data = {
-            "spells": {},
-            "total": {
-                "count": 0,
-                "0": 0,
-                "25": 0,
-                "50": 0,
-                "75": 0,
-            }
-        }
+        data = {}
 
         query = gql("""
-            query ($zoneID: Int!, $encounterID: Int!, $sourceClass: String!, $limit: Int!, $page: Int!) {
+            query ($zoneID: Int!, $encounterID: Int!, $startTime: Float!, $limit: Int!, $page: Int!) {
               reportData {
-                reports(zoneID: $zoneID, guildServerRegion: "EU", limit: $limit, page: $page) {
+                reports(zoneID: $zoneID, guildServerRegion: "EU", startTime: $startTime, limit: $limit, page: $page) {
                   data {
                     title
-                    events(encounterID: $encounterID, sourceClass: $sourceClass, dataType: DamageDone, startTime: 0, endTime: 999999999, limit: 400) {
+                    events(encounterID: $encounterID, startTime: 0, endTime: 999999999, limit: 20000) {
                        data
                     }
                   }
@@ -40,7 +31,7 @@ class Analyzer:
             result = self.client.execute(query, {
                 "zoneID": zoneID,
                 "encounterID": encounterID,
-                "sourceClass": sourceClass,
+                "startTime": startTime,
                 "limit": limit,
                 "page": p,
             })
@@ -51,23 +42,32 @@ class Analyzer:
             for report in result["reportData"]["reports"]["data"]:
                 re = self.analyzeReport(report, spellFilter)
 
-                for spell_id in re["spells"]:
-                    if spell_id not in data["spells"]:
-                        data["spells"][spell_id] = re["spells"][spell_id]
-                    else:
-                        for key in re["spells"][spell_id]:
-                            if key != "spellName":
-                                data["spells"][spell_id][key]+= re["spells"][spell_id][key]
+                if "total" not in data:
+                    data = re
+                else:
+                    for spell_id in re["spells"]:
+                        if spell_id not in data["spells"]:
+                            data["spells"][spell_id] = re["spells"][spell_id]
+                        else:
+                            for index, dataset in enumerate(re["spells"][spell_id]["datasets"]):
+                                for key in dataset:
+                                    if key != "coe":
+                                        data["spells"][spell_id]["datasets"][index][key]+= dataset[key]
 
-                for key in re["total"]:
-                    data["total"][key]+= re["total"][key]
+                    for index, dataset in enumerate(re["total"]):
+                        for key in dataset:
+                            if key != "coe":
+                                data["total"][index][key]+= dataset[key]
 
         # Summarize
-        for spell_id in data["spells"]:
-            data["spells"][spell_id]["mitigation"] = str(round(100 - 100*(data["spells"][spell_id]["75"] * 0.25 + data["spells"][spell_id]["50"] * 0.5 + data["spells"][spell_id]["25"] * 0.75 + data["spells"][spell_id]["0"])/data["spells"][spell_id]["count"], 2))+"%"
+        if "total" in data:
+            for spell_id in data["spells"]:
+                for dataset in data["spells"][spell_id]["datasets"]:
+                    dataset["mitigation"] = str(round(100 - 100*(dataset["75"] * 0.25 + dataset["50"] * 0.5 + dataset["25"] * 0.75 + dataset["0"])/dataset["count"], 2))+"%"
 
-        if data["total"]["count"] > 0:
-            data["total"]["mitigation"] = str(round(100 - 100*(data["total"]["75"] * 0.25 + data["total"]["50"] * 0.5 + data["total"]["25"] * 0.75 + data["total"]["0"])/data["total"]["count"], 2))+"%"
+            for dataset in data["total"]:
+                if dataset["count"] > 0:
+                    dataset["mitigation"] = str(round(100 - 100*(dataset["75"] * 0.25 + dataset["50"] * 0.5 + dataset["25"] * 0.75 + dataset["0"])/dataset["count"], 2))+"%"
 
         if format == "csv":
             return self.toCsv(data)
@@ -77,18 +77,37 @@ class Analyzer:
     def analyzeReport(self, report, spellFilter):
         data = {
             "spells": {},
-            "total": {
-                "count": 0,
-                "0": 0,
-                "25": 0,
-                "50": 0,
-                "75": 0
-            }
+            "total": [
+                {
+                    "coe": False,
+                    "count": 0,
+                    "0": 0,
+                    "25": 0,
+                    "50": 0,
+                    "75": 0
+                },
+                {
+                    "coe": True,
+                    "count": 0,
+                    "0": 0,
+                    "25": 0,
+                    "50": 0,
+                    "75": 0
+                }
+            ]
         }
 
         primarySpells = [27215, 32231, 27209, 27070, 30451, 27074]
+        coe_active = False
+        coe = 27228
 
         for event in report["events"]["data"]:
+            if event["type"] == "applydebuff" and event["abilityGameID"] == coe:
+                coe_active = True
+
+            if event["type"] == "removedebuff" and event["abilityGameID"] == coe:
+                coe_active = False
+
             if (event["type"] == "damage" and
                 "tick" not in event and
                 "sourceMarker" not in event and
@@ -101,11 +120,24 @@ class Analyzer:
                 if event["abilityGameID"] not in data["spells"]:
                     data["spells"][event["abilityGameID"]] = {
                         "spellName": self.spellName(event["abilityGameID"]),
-                        "count": 0,
-                        "0": 0,
-                        "25": 0,
-                        "50": 0,
-                        "75": 0
+                        "datasets": [
+                            {
+                                "coe": False,
+                                "count": 0,
+                                "0": 0,
+                                "25": 0,
+                                "50": 0,
+                                "75": 0
+                            },
+                            {
+                                "coe": True,
+                                "count": 0,
+                                "0": 0,
+                                "25": 0,
+                                "50": 0,
+                                "75": 0
+                            }
+                        ]
                     }
 
                 if event["hitType"] == 16:
@@ -113,10 +145,11 @@ class Analyzer:
                 else:
                     key = "0"
 
-                data["spells"][event["abilityGameID"]]["count"]+= 1
-                data["total"]["count"]+= 1
-                data["spells"][event["abilityGameID"]][key]+= 1
-                data["total"][key]+= 1
+                coe_key = int(coe_active)
+                data["spells"][event["abilityGameID"]]["datasets"][coe_key]["count"]+= 1
+                data["spells"][event["abilityGameID"]]["datasets"][coe_key][key]+= 1
+                data["total"][coe_key]["count"]+= 1
+                data["total"][coe_key][key]+= 1
 
         return data
 
@@ -149,17 +182,21 @@ class Analyzer:
         return "Unknown "+key;
 
     def toCsv(self, data, d = "\t"):
-        csv = "Spell"+d+"Hits"+d+"0%"+d+"25%"+d+"50%"+d+"75%"+d+"Mitigation\n"
+        csv = "Spell"+d+"CoE"+d+"Hits"+d+"0%"+d+"25%"+d+"50%"+d+"75%"+d+"Mitigation\n"
 
-        for spell_id in data["spells"]:
-            for key in data["spells"][spell_id]:
-                csv+= str(data["spells"][spell_id][key])+d
-            csv = csv[0:-1]+"\n"
+        if "total" in data:
+            for spell_id in data["spells"]:
+                for dataset in data["spells"][spell_id]["datasets"]:
+                    csv+= str(data["spells"][spell_id]["spellName"])+d
+                    for key in dataset:
+                        csv+= str(dataset[key])+d
+                    csv = csv[0:-1]+"\n"
 
-        csv+= "Total"+d;
-        for key in data["total"]:
-            csv+= str(data["total"][key])+d
-        csv = csv[0:-1]+"\n"
+            for dataset in data["total"]:
+                csv+= "Total"+d
+                for key in dataset:
+                    csv+= str(dataset[key])+d
+                csv = csv[0:-1]+"\n"
 
         return csv
 
